c30bfcc792092d82513cb6bf3f0d78743efabe10
Divyesh Kotadia <131876498+Divyesh3899@users.noreply.github.com>
2023-05-04 18:15:51 +0530

    Add files via upload

---
+++ Assigment/module -2/even or odd.py
@@ -0,0 +1,11 @@
+#Write a Python program to find whether a given number is even or odd,print out an appropriate message to the user.
+
+
+num=int(input("Enter a Number: "))
+
+
+if num % 2 == 0:
+    print(num,"even")
+
+else:
+    print(num,"odd")
